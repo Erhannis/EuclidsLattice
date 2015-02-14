a5e9d5cf6a184415ceb67ff53df04ac3ccbc58b5
erhannis <Eyeillus@gmail.com>
2015-02-14 00:16:43 -0800

    Weak attempts to repair SAF absence.

--- src/latticetestwork/RemoteWorker.java
+++ src/latticetestwork/RemoteWorker.java
@@ -8,15 +8,11 @@ import java.awt.Color;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
 import java.util.logging.Logger;
 import javax.net.ServerSocketFactory;
-import org.jdesktop.application.Application;
 
 /**
  *
